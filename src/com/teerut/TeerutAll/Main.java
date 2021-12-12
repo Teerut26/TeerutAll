67a2f0cc2a71198c5caadba94d8c2733c9ef2e7b
Teerut26 <teerutsr@gmail.com>
2021-12-12 22:04:20 +0700

    1.0.3 remove discord web hook

--- src/com/teerut/TeerutAll/Main.java
+++ src/com/teerut/TeerutAll/Main.java
@@ -1,8 +1,6 @@
 package com.teerut.TeerutAll;
 
 import com.teerut.TeerutAll.ConcreteWater.ConcreteWaterEvent;
-import com.teerut.TeerutAll.Discord.DiscordEvents;
-import com.teerut.TeerutAll.Discord.DiscordWebhook;
 import com.teerut.TeerutAll.Fishing.FishEvents;
 import com.teerut.TeerutAll.GetPlayerLocation.GetPlayerLocation;
 import com.teerut.TeerutAll.ItemMerge.ItemMergeEvents;
@@ -23,21 +21,6 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ConcreteWaterEvent(this), this);
         getServer().getPluginManager().registerEvents(new ItemMergeEvents(this), this);
         getServer().getPluginManager().registerEvents(new FishEvents(this), this);
-        getServer().getPluginManager().registerEvents(new DiscordEvents(this), this);
-
-        DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/881736314028982292/dx7rKUyrH994PN36oJ7R6Yy-popLUP4qPZyZBInNE6hK4LdFIdTJ5FHgRlCJdhTIAtAK");
-        webhook.setAvatarUrl("https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP");
-        webhook.addEmbed(new DiscordWebhook.EmbedObject()
-                .setTitle("Server Open")
-                .setColor(Color.GREEN)
-        );
-        webhook.setUsername("Minecraft Server");
-
-        try {
-            webhook.execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         System.out.println(ChatColor.AQUA + "[TeerutAll] : onEnable");
         super.onEnable();
@@ -46,19 +29,6 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         System.out.println(ChatColor.AQUA + "[TeerutAll] : onDisable");
-        DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/881736314028982292/dx7rKUyrH994PN36oJ7R6Yy-popLUP4qPZyZBInNE6hK4LdFIdTJ5FHgRlCJdhTIAtAK");
-        webhook.setAvatarUrl("https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP");
-        webhook.addEmbed(new DiscordWebhook.EmbedObject()
-                .setTitle("Server Off")
-                .setColor(Color.RED)
-        );
-        webhook.setUsername("Minecraft Server");
-
-        try {
-            webhook.execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         super.onDisable();
     }
 
